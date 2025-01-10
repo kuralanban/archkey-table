c9c7d14f9cab0c70a2852819894ea27f6c1b5e46
kuralanban <94080302+kuralanban@users.noreply.github.com>
2025-01-10 17:03:30 +0530

    Implemeted re-usable table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ReusableTableComponent } from './reusable-table/reusable-table.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTableModule } from '@angular/material/table';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReusableTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatIconModule,
+    MatExpansionModule,
+    FormsModule,
+    AppRoutingModule,
+    MatButtonToggleModule,
+    MatTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reusable-table/reusable-table.component.ts
@@ -0,0 +1,59 @@
+import {Component, Input, OnChanges, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-reusable-table',
+  templateUrl: './reusable-table.component.html',
+  styleUrls: ['./reusable-table.component.css'],
+})
+export class ReusableTableComponent <T extends { [key: string]: any }> implements OnInit,OnChanges {
+  @Input() data: T[] = [];
+  @Input() columns: any[] = [];
+  @Input() expandableKey: string = '';
+  @Input() selectedRowSize: string = '';
+  @Input() selectedColumnSize: string = '';
+  @Input() stickyHeader = false;
+  @Input() stickyRowIndices: number[] = [];
+  @Input() stickyColumns: string[] = [];
+
+  rowHeight: number = 40;
+  columnsToDisplay: string[] = [];
+  dataSource!: MatTableDataSource<T>;
+  expandedElement!: any;
+  
+  
+  ngOnInit(): void {
+    this.columnsToDisplay = this.columns ? 
+    this.columns.map(col => col.key).filter(col => col !== this.expandableKey) : [];
+    if (!this.columnsToDisplay.includes('action')) {
+      this.columnsToDisplay.push('action');
+    }
+    this.dataSource = new MatTableDataSource(this.data);
+  }
+  ngOnChanges(): void {
+    console.log(this.selectedRowSize)
+    this.updateRowHeight();
+  }
+  
+  updateRowHeight(): void {
+    switch (this.selectedRowSize) {
+      case 'small-size':
+        this.rowHeight = 10;
+        break;
+      case 'large-size':
+        this.rowHeight = 70;
+        break;
+      default:
+        this.rowHeight = 50; 
+    }
+  }
+  isStickyRow(row: any): boolean {
+    const rowIndex = this.dataSource.data.indexOf(row);
+    return this.stickyRowIndices.includes(rowIndex);
+  }
+  isStickyColumn(column: string): boolean {
+    return this.stickyColumns.includes(column);
+  }
+  
+}
